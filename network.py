fix(fit): converge badly due to network init as 0, choke gradient

network.py
@@ -1,5 +1,6 @@
 import jax
 import jax.numpy as jnp
+import jaxtyping
 import equinox as eqx
 
 
@@ -42,14 +43,10 @@ def TVLoss1D(x, scale, window_size=1):
 # Kim, Suyong, et al. "Stiff neural ordinary differential equations."
 class ScaleMLP(eqx.Module):
     mlp: eqx.nn.MLP
-    yMin: jax.Array
-    yscale: jax.Array
-    ytscale: jax.Array
 
     def __init__(
             self,
             data_size: int,
-            scale: dict[str, jax.typing.ArrayLike],
             width_size: int,
             depth: int,
             key,
@@ -57,7 +54,7 @@ class ScaleMLP(eqx.Module):
         """
         Args:
             num_spc: number of species
-            scale: dict of concentration and time scales
+            scale: time series scale statistics
         """
         super().__init__()
 
@@ -69,12 +66,8 @@ class ScaleMLP(eqx.Module):
             key=key,
         )
 
-        self.yMin = scale['yMin']
-        self.yscale = scale['yScale']
-        self.ytscale = scale['ytScale']
-
-    def __call__(self, t: jax.Array, y: jax.Array) -> jax.Array:
-        y = (y - self.yMin) / self.yscale
-        y = self.mlp(y)
-        dy_dt = y * self.ytscale
+    def __call__(self, y: jax.Array, scale: dict) -> jax.Array:
+        y_safe = jnp.clip(y, 0.0)
+        y_scaled = y_safe / scale['yScale']
+        dy_dt = self.mlp(y_scaled) * scale['ytScale']
         return dy_dt

traj_fit.py
@@ -1,3 +1,8 @@
+# Usage: python traj_fit.py 
+#           --config <config_path>
+#           --target <yaml_target>
+#           --log/--no-log
+
 from pathlib import Path
 
 import jax
@@ -8,6 +13,7 @@ import optax
 import tqdm
 from torch.utils.data import DataLoader
 import orbax.checkpoint as ocp
+import numpy as np
 
 import chem_data as cd
 import model
@@ -16,46 +22,50 @@ import train_utils
 import plots.plot as pp
 
 
+jax.config.update("jax_enable_x64", True)
 cfg = train_utils.load_config()
 
 # Data =========================================================================
 
-sch = cd.ROBER()
+sch = cd.POLLU()
 
-y_arr, t_arr = sch.data(1)
+def MAD(x, axis=None):
+    m = jnp.median(x, axis=axis, keepdims=True)
+    return jnp.median(jnp.abs(x - m), axis=axis)
+
+y_arr, t_arr = sch.data(1, t=np.linspace(0, 60, 20))
+print(t_arr)
 scale = {
     'yMax' : jnp.max(y_arr, axis=(0,1)),
     'yMin' : jnp.min(y_arr, axis=(0,1)),
-    'tScale' : t_arr[0,-1] - t_arr[0,0],  # same tscale for every traj
+    'tScale' : jnp.asarray(t_arr[0,-1] - t_arr[0,0]),  # same tscale for every traj
 }
 scale['yScale'] = jnp.where(scale['yMax']-scale['yMin'] == 0.0, scale['yMax'], scale['yMax']-scale['yMin'])
 scale['ytScale'] = scale['yScale'] / scale['tScale']
-print("scale", scale)
+scale['median'] = jnp.median(y_arr, axis=(0,1))
+scale['deviation'] = 1.4826 * MAD(y_arr, axis=(0,1))
+print(scale)
 
 # Model ========================================================================
 
 neural_network = nn.ScaleMLP(
-    sch.num_spc, scale,
-    width_size=128, depth=3, key=jax.random.PRNGKey(cfg['seed'])
+    data_size=sch.num_spc, width_size=10, depth=4,
+    key=jax.random.PRNGKey(cfg['seed']),
 )
+
+params = {
+    'solver': cfg['solver'],
+    'scale': scale
+}
 
 def mse(pred, target):
     return jnp.mean((pred - target) ** 2)
 
-def scale_mse(pred, target, *, dim):
-    """Scale loss by the maximum in 'dim' dimension"""
-    scale = jnp.max(target, axis=dim, keepdims=True)
+def scale_mse(pred, target, scale):
     return jnp.mean(((pred - target) / scale) ** 2)
 
-def loss_fn(neu_net, batch):
-    params = {
-        'neural_network': neu_net,
-        'solver': {
-            'rtol': cfg['solver']['rtol'],
-            'atol': cfg['solver']['atol'],
-            'max_steps': cfg['solver']['max_steps'],
-        },
-    }
+def loss_fn(neu_net, params, batch):
+    params['neural_network'] = neu_net
     inputs = {
         'y0': batch['conc'][:,0,:],
         'ts': batch['time'],
@@ -63,17 +73,17 @@ def loss_fn(neu_net, batch):
     traj_pred = eqx.filter_vmap(model.forward, in_axes=(None,0,None))(
         params, inputs, model.neural_ode
     )
-    return scale_mse(traj_pred, batch['conc'], dim=-2)
+    return scale_mse(traj_pred, batch['conc'], params['scale']['yScale'])
 
 @eqx.filter_jit
 def opt_step(
         optim: optax.GradientTransformation, opt_state: jaxtyping.PyTree,
-        neu_net, batch
+        neu_net, params, batch
     ):
     loss_val, grads = eqx.filter_value_and_grad(loss_fn)(
-        neu_net, batch
+        neu_net, params, batch
     )
-    updates, opt_state = optim.update(grads, opt_state)
+    updates, opt_state = optim.update(grads, opt_state, neu_net)
     new_neu_net = eqx.apply_updates(
         neu_net, updates
     )
@@ -82,20 +92,24 @@ def opt_step(
 # Training =====================================================================
 checkpointer = ocp.StandardCheckpointer()
 ckpt_path = Path('checkpoints/').absolute()
-# ckpt_path = ocp.test_utils.erase_and_create_empty(ckpt_path)
-ckpt_idx = 2
+ckpt_path = ocp.test_utils.erase_and_create_empty(ckpt_path)
+ckpt_idx = 0
 
 # load checkpoint
-state, static = eqx.partition(neural_network, eqx.is_array_like)
-abstract_state = jax.tree.map(ocp.utils.to_shape_dtype_struct, state)
-restored_state = checkpointer.restore(ckpt_path / f"{ckpt_idx}", abstract_state)
-neural_network = eqx.combine(restored_state, static)
+# state, static = eqx.partition(neural_network, eqx.is_array_like)
+# abstract_state = jax.tree.map(ocp.utils.to_shape_dtype_struct, state)
+# restored_state = checkpointer.restore(ckpt_path / f"{ckpt_idx}", abstract_state)
+# neural_network = eqx.combine(restored_state, static)
 
-optimizer = optax.adam(cfg['learning_rate'])
+optimizer = optax.chain(
+    optax.clip(10),
+    optax.adamw(cfg['learning_rate']),
+    # optax.contrib.reduce_on_plateau(factor=0.5, patience=100),
+)
 opt_state = optimizer.init(eqx.filter(neural_network, eqx.is_inexact_array))
 
-length_strategy = [(jnp.asarray(0), end) for end in jnp.arange(0.3, 1, 0.1)]
-epochs_strategy = (jnp.ones(len(length_strategy)) * 0.5 * cfg['epochs']).astype(int)
+length_strategy = [(jnp.asarray(0), end) for end in jnp.asarray([1.0])]
+epochs_strategy = (jnp.ones(len(length_strategy)) * cfg['epochs']).astype(int)
 for (start, end), epochs in zip(length_strategy, epochs_strategy):
     print(f"trajectory slice: {start:.2f} {end:.2f}")
     start_idx = (y_arr.shape[1] * start).astype(int)
@@ -112,33 +126,28 @@ for (start, end), epochs in zip(length_strategy, epochs_strategy):
         for batch in traj_dataloader:
             neural_network, loss_val, opt_state = opt_step(
                 optimizer, opt_state,
-                neural_network, batch
+                neural_network, params, batch
             )
         bar.set_postfix({
             'loss': f"{float(jnp.squeeze(loss_val)):.4e}",
         })
 
-    # checkpoint
-    model_state, _ = eqx.partition(neural_network, eqx.is_array_like)
-    ckpt_idx += 1
-    checkpointer.save(ckpt_path / f"{ckpt_idx}", model_state)
+        if i % 500 == 0:
+            # checkpoint
+            model_state, _ = eqx.partition(neural_network, eqx.is_array_like)
+            ckpt_idx += 1
+            checkpointer.save(ckpt_path / f"{ckpt_idx}", model_state)
 
-    # Testing
-    params = {
-        'neural_network': neural_network,
-        'solver': {
-            'rtol': cfg['solver']['rtol'],
-            'atol': cfg['solver']['atol'],
-            'max_steps': cfg['solver']['max_steps'],
-        },
-    }
-    inputs = {
-        'y0': y_arr[0, start_idx],
-        'ts': t_arr[0, start_idx:end_idx],
-    }
-    traj_pred = model.forward(params, inputs, model.neural_ode)
-    traj_true = y_arr[0, start_idx:end_idx]
-    print(traj_pred)
-    print(traj_true)
-    fig = pp.plot_series(y=traj_pred, t=inputs['ts'], yy=traj_true, tt=inputs['ts'])
-    fig.savefig(f"plots/traj_fit_{start_idx:.1f}-{end_idx:.1f}.png", dpi=300)
+        if i % 100 == 0:
+            # Testing
+            inputs = {
+                'y0': y_arr[0, start_idx],
+                'ts': t_arr[0, start_idx:end_idx],
+            }
+            traj_pred = model.forward({**params, **{'neural_network': neural_network}}, inputs, model.neural_ode)
+            traj_true = y_arr[0, start_idx:end_idx]
+            print(traj_pred[-1])
+            print(traj_true[-1])
+            print(scale_mse(traj_pred, traj_true, params['scale']['yScale']))
+            fig = pp.plot_series(y=traj_pred, t=inputs['ts'], yy=traj_true, tt=inputs['ts'])
+            fig.savefig(f"plots/traj_fit_{start_idx:.1f}-{end_idx:.1f}.png", dpi=300)